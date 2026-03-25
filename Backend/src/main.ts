6db182a888985887b433526fdc2f5d25284ce944
Raj Majunder <rajm97985@gmail.com>
2026-03-25 22:20:35 +0530

    fix cors error

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -9,7 +9,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
